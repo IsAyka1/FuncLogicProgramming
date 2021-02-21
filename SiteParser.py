266379f9c8db5217e650829e56e4aac621fe19fd
IsAyka1 <aigul-isl@yandex.ru>
2021-02-21 21:17:07 +0300

    RandomData

--- SiteParser.py
+++ SiteParser.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import requests as req
+import random
 
 
 def site_parser():
     # resp = req.get("https://github.com/rmcelreath/rethinking/blob/master/data/Howell1.csv")
-    # soup = BeautifulSoup(resp.text, 'lxml')
-    # root = soup.body
-    # outputFileName = 'siteParserOutput.txt'
-    # with open(outputFileName, 'w') as file:
-    #     for tag in soup.find_all("td", {"class": "blob-code blob-code-inner js-file-line"}):
-    #         file.write('{data}\n'.format(data=tag.text))
-    #  need shuffle all data!!!
-    outputFileName = 'NewData.txt'
+    resp = req.get("https://github.com/IsAyka1/FuncLogicProgramming/blob/master/NewData.txt")
+    soup = BeautifulSoup(resp.text, 'lxml')
+    root = soup.body
+    outputFileName = 'RandomData.txt'
+    with open(outputFileName, 'w') as file:
+        list = []
+        for tag in soup.find_all("td", {"class": "blob-code blob-code-inner js-file-line"}):
+	    list.append(tag.text)
+        file.write(random.shuffle(list))
     return outputFileName
 
 
